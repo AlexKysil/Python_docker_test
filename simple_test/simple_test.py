4947fa12806b3cef17f8b558a662be6029e11218
AlexKysil <alex.kisel0504@gmail.com>
2019-01-23 18:06:36 +0200

    correct some mistakes

--- simple_test/simple_test.py
+++ simple_test/simple_test.py
@@ -1,15 +1,22 @@
 from selenium import webdriver
+import unittest
 
+class test_simple(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
 
-def simple_test():
-    driver = webdriver.Chrome()
-    driver.get('https://www.google.com')
-    expected_string = 'The Beatles - Something - YouTube'
-    google_search_line = driver.find_element_by_name('q')
-    google_search_line.send_keys("something")
-    google_search_line.submit()
-    actual_string = driver.find_element_by_xpath("//div[@class='kp-header']//h3//h3").text
-    assert(actual_string == expected_string)
-    driver.quit()
+    def test(self):
+        driver = self.driver
+        driver.get('https://www.google.com')
+        expected_string = 'The Beatles - Something - YouTube'
+        google_search_line = driver.find_element_by_name('q')
+        google_search_line.send_keys("something")
+        google_search_line.submit()
+        actual_string = driver.find_element_by_xpath("//div[@class='kp-header']//h3//h3").text
+        assert(actual_string == expected_string)
 
-simple_test()
+    def teardown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
