18668e2804ee4d3ce7ac2b968ac95cdf6f8e811d
AlexKysil <alex.kisel0504@gmail.com>
2019-01-24 15:54:43 +0200

    refactor  using pytest

--- simple_test/simple_test.py
+++ simple_test/simple_test.py
@@ -1,27 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import unittest
+import pytest
 
-class test_simple(unittest.TestCase):
-    def setUp(self):
+@pytest.fixture(scope="class")
+def setup(request):
         chrome_options = Options()
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        self.driver = webdriver.Chrome(chrome_options = chrome_options)
 
-    def test(self):
-        driver = self.driver
-        driver.get('https://www.google.com')
+        driver = webdriver.Chrome(options = chrome_options)
+
+        request.cls.driver = driver
+        yield driver
+        driver.quit()
+
+@pytest.mark.usefixtures("setup")
+class Test_simple:
+
+    def test_one(self):
+        self.driver.get('https://www.google.com')
         expected_string = 'The Beatles - Something - YouTube'
-        google_search_line = driver.find_element_by_name('q')
+        google_search_line = self.driver.find_element_by_name('q')
         google_search_line.send_keys("something")
         google_search_line.submit()
-        actual_string = driver.find_element_by_xpath("//div[@class='FGpTBd']//h3//h3").text
+        actual_string = self.driver.find_element_by_xpath("//div[@class='FGpTBd']//h3//h3").text
         assert(actual_string == expected_string)
-
-    def teardown(self):
-        self.driver.quit()
-
-if __name__ == '__main__':
-    unittest.main()
