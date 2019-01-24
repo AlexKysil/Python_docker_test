a0b7e0bff36a879be85341eab54b0c24f7472395
AlexKysil <alex.kisel0504@gmail.com>
2019-01-24 13:58:38 +0200

    working file

--- simple_test/simple_test.py
+++ simple_test/simple_test.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import unittest
 
 class test_simple(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome(chrome_options = chrome_options)
 
     def test(self):
         driver = self.driver
@@ -12,7 +17,7 @@ class test_simple(unittest.TestCase):
         google_search_line = driver.find_element_by_name('q')
         google_search_line.send_keys("something")
         google_search_line.submit()
-        actual_string = driver.find_element_by_xpath("//div[@class='kp-header']//h3//h3").text
+        actual_string = driver.find_element_by_xpath("//div[@class='FGpTBd']//h3//h3").text
         assert(actual_string == expected_string)
 
     def teardown(self):
